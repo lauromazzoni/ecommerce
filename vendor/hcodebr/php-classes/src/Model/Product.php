7a61714d4d36a78822b4d07ebde5b0cbf1e1d231
Lauro Mazzoni <lauromazzoni@gmail.com>
2020-01-01 16:29:26 -0300

    Detalhes do produto

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -131,5 +131,36 @@ class Product extends Model {
 		$this->checkPhoto();	
 	}
 
+	public function getFromUrl($desurl){
+
+		$sql = new Sql();
+
+		$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
+			":desurl"=>$desurl
+		]);
+
+		//AO INVÉS DE USAR O "return", PEGOU-SE A LINHA RETORNADA PELA QUERY E A INSERIU NO OBJETO COM O "setData()", ASSIM, NO ARQUIVO site.php, DENTRO DE "$app->get("/products/:desurl", function($desurl)){", O OBJETO "$product" JÁ ESTÁ COM OS DADOS DA QUERY. DAÍ, O PRÓPRIO OBJETO "$product" PEGA OS VALORES ("getValues()") E PASSA PARA O TEMPLATE
+		$this->setData($rows[0]);
+	}
+
+	public function getCategories(){
+
+		$sql = new Sql();
+
+		return $sql->select("
+				SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
+			", [
+				':idproduct'=>$this->getidproduct()
+			]);
+
+
+
+
+	}
+
+
+
+
+
 }
 ?>
