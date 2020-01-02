9bb2d5f896fdc6b91ecd0012d2e4ac0573a2c12c
Lauro Mazzoni <lauromazzoni@gmail.com>
2020-01-02 11:21:41 -0300

    Carrinho Produtos - add

--- site.php
+++ site.php
@@ -71,11 +71,71 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
-})
+	$page->setTpl("cart", [
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
+});
 
 
+//o $idproduct na function (lina logo abaixo) é o parâmetro informado na url
+$app->get("/cart/:idproduct/add", function($idproduct){
 
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	//recuperando o carrinho da sessão, se não existir ele cria um novo
+	$cart = Cart::getFromSession();
+
+	//se o "qtd" for informado no $_GET, o valor será o cast para int da variável recebida "qtd". Se o "qtd" não foi definido, o padrão é 1
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i = 0; $i < $qtd; $i++){
+		$cart->addProduct($product); //chama o método addProduct quantas vezes forem necessárias (de acordo com o $qtd da linha acima)
+	}
+
+	header("Location: /cart");
+	exit;
+
+});
+
+
+//o "minus" na url abaixo é só para remover um do carrinho
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	//recuperando o carrinho da sessão
+	$cart = Cart::getFromSession();
+
+	//o removeProduct recebendo somente o $product irá remover somente um, já que o segundo parâmentro está sendo passado como false (padrão)
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+//remove todos os produtos do carrinho
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	//recuperando o carrinho da sessão, se não existir ele cria um novo
+	$cart = Cart::getFromSession();
+
+	//está passando o true para a função pois irá remover todos
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
 
 
 
